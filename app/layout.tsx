692c2f1016e15537676a2ec3d972c228bebb648f
rishavroy009 <rishavgrd1@gmail.com>
2024-04-02 18:53:19 +0530

    first draft commit

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const poppins = Poppins({
+   subsets: ["latin"] ,
+   weight:["400", "500", "600", "700"],
+   variable:"--font-poppins",
+  });
+
+export const metadata: Metadata = {
+  title: "Evently",
+  description: "Taking a step ahead for the best event management era..",
+  icons:{
+    icon:'/assets/images/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={poppins.variable}>{children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
